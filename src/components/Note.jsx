1224c2b01a23354c3278339da1f81490051e3725
terrencecoding <126296863+terrencecoding@users.noreply.github.com>
2023-07-18 15:17:07 -0700

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note({ title, content, onDelete, id }) {
+  return (
+    <div className="note">
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <button
+        onClick={() => {
+          onDelete(id);
+        }}
+      >
+        DELETE
+      </button>
+    </div>
+  );
+}
+
+export default Note;
